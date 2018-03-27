38d2a1e969a951ac2595c1fb86f9da14c820f76f
Suraj <sbsurajbhat@gmail.com>
2018-03-27 09:17:32 +0530

    Add: Flask files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def index():
+	return render_template("home.html")
+
+
+
+
+
+if(__name__=="__main__"):
+	app.run(debug=True)
